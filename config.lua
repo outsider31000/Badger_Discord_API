0a9fb000b4b67c3fc04cfe3156a867d57f8c43ba
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-09 19:22:44 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,25 @@
 Config = {
 	Guild_ID = '',
 	Bot_Token = '',
-	RoleList = {},
+	RoleList = {
+		["Member"] = 929164998977159168, --name of the role , id of the role
+		["Founder"] = 929362278401343489,
+		["Admin"] = 930111658741858324,
+		["HeadAdmin"] = 705303420826943518,
+		["Moderator"] = 972587855538970674,
+		["Donator"] = 705304120264884244,
+
+	},
 	CacheDiscordRoles = true, -- true to cache player roles, false to make a new Discord Request every time
 	CacheDiscordRolesTime = 60, -- if CacheDiscordRoles is true, how long to cache roles before clearing (in seconds)
 }
-
+-- adaptive card will display when joining your server  with buttons to join discord or visit a link like youtube or website before letting you join the server
 Config.Splash = {
 	Header_IMG = 'https://forum.cfx.re/uploads/default/original/3X/a/6/a6ad03c9fb60fa7888424e7c9389402846107c7e.png',
 	Enabled = true,
 	Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
 	Heading1 = "Welcome to [ServerName]",
 	Heading2 = "Make sure to join our Discord and check out our website!",
-	Discord_Link = 'https://discord.gg',
-	Website_Link = 'https://badger.store',
+	Discord_Link = '',
+	Website_Link = '',
 }
